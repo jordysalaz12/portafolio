bbf888acabbd040a87d6a4d6aa4f47008385e036
Jordy Salazar <jordysalazar350@gmail.com>
2024-07-05 18:06:39 -0500

    aea idiota

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const navAparecer=document.querySelector(".nav-elementos")
+
+// const navAparecer=document.querySelector(".nav-elementos")
 
 
-const buttonToogle= document.querySelector("#button-toggle");
- buttonToogle.addEventListener("click",function(){
- navAparecer.classList.toggle("active");
- }) 
-
+// const buttonToogle= document.querySelector("#button-toggle");
+//  buttonToogle.addEventListener("click",function(){
+//  navAparecer.classList.toggle("active");
+//  }) 
 
  
+
+ document.addEventListener("DOMContentLoaded", function() {
+    const navAparecer = document.querySelector(".nav-elementos");
+    const buttonToogle = document.querySelector("#button-toggle");
+
+    buttonToogle.addEventListener("click", function() {
+        navAparecer.classList.toggle("active");
+    });
+});
